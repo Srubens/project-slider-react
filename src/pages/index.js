0d1488ea04b13de817cc7a890423dd0dfe6a6d6e
Rubens Filipe <rubensfilipe6@gmail.com>
2023-02-02 23:38:02 -0300

    add images modificando index add data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,30 @@
-function Index() {
-    return (
-        <div>ola mundo!</div>
-    );
+import React, {useState, useEffect} from 'react'
+import data from './data.js'
+const Index = () =>{
+    const [people, setPeople] = useState(data)
+    const [index, setIndex] = useState(0)
+    return(
+        <section>
+            <div>
+                <h2>Reviews</h2>
+            </div>
+            <div className="slider">
+                {
+                    people.map((person,personIndex) =>{
+                        const {id, image, name, job, text} = person
+                        return (
+                            <article key={id}>
+                                <img src={image} alt={name} />
+                                <h4>{name}</h4>
+                                <p>{job}</p>
+                                <p>{text}</p>
+                            </article>
+                        )
+                    })
+                }
+            </div>
+        </section>
+    )
 }
 
-export default Index;
+export default Index
