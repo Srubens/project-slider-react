fd8919beaacb9b4031237aff49cc029a431b56af
Rubens Filipe <rubensfilipe6@gmail.com>
2023-02-03 23:57:45 -0300

    ajustando o css

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,26 +4,28 @@ const Index = () =>{
     const [people, setPeople] = useState(data)
     const [index, setIndex] = useState(0)
     return(
-        <section>
-            <div>
-                <h2>Reviews</h2>
-            </div>
-            <div className="slider">
-                {
-                    people.map((person,personIndex) =>{
-                        const {id, image, name, job, text} = person
-                        return (
-                            <article key={id}>
-                                <img src={image} alt={name} />
-                                <h4>{name}</h4>
-                                <p>{job}</p>
-                                <p>{text}</p>
-                            </article>
-                        )
-                    })
-                }
-            </div>
-        </section>
+        <div className="container">
+            <section className='slider' >
+                <div>
+                    <h2>Reviews</h2>
+                </div>
+                <div className="slider-center">
+                    {
+                        people.map((person,personIndex) =>{
+                            const {id, image, name, job, text} = person
+                            return (
+                                <article key={id}>
+                                    <img src={image} alt={name} />
+                                    <h4>{name}</h4>
+                                    <p>{job}</p>
+                                    <p>{text}</p>
+                                </article>
+                            )
+                        })
+                    }
+                </div>
+            </section>
+        </div>
     )
 }
 
